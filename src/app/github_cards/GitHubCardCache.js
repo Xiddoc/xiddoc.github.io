327007c1e6c9e9fa636bbe0d54252208a75b7ee7
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-01-15 23:53:34 +0200

    Fixed cache bugs

--- src/app/github_cards/GitHubCardCache.js
+++ src/app/github_cards/GitHubCardCache.js
@@ -2,7 +2,8 @@ const fetch_hook = require("react-repo-card/dist/hooks/fetch");
 const {useState} = require("react");
 
 // Configurable
-const CACHE_TIMEOUT = 60000;
+const CACHE_TIMEOUT = 60 * 60 * 1000;
+const REQ_TIMEOUT = 10 * 1000;
 
 // Create function to use caching for URL fetches
 function useCacheFetch(url) {
@@ -11,12 +12,17 @@ function useCacheFetch(url) {
   // Get old data
   const prevResp = JSON.parse(localStorage.getItem(url));
 
-  const [response, setResponse] = useState(prevResp === null ? {} : prevResp.data);
-  const [loading, setLoading] = useState(true);
+  // Return hooks
+  const isLoading = prevResp === null || prevResp.data === null;
+  const [response, setResponse] = useState(isLoading ? {} : prevResp.data);
+  const [loading, setLoading] = useState(isLoading);
   const [hasError, setHasError] = useState(false);
 
   // If we did not find old data, or the cache is too old
-  if (prevResp === null || Math.abs(now - prevResp.time) > CACHE_TIMEOUT) {
+  if (prevResp === null || Math.abs(now - prevResp.time) > CACHE_TIMEOUT ||
+      (Math.abs(now - prevResp.time) > REQ_TIMEOUT && prevResp.data === null)) {
+    // Prevent race condition which triggers rate limit
+    localStorage.setItem(url, JSON.stringify({time: now, data: null}));
     // Download new data
     fetch(url).then(
       async (resp) => {
