922ff9b77652bd0ccebd1381e26819b12688a890
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-01-16 00:15:10 +0200

    Better emoji background parallax

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {Parallax, ParallaxLayer} from "@react-spring/parallax";
 import RandomLogo from "./logo/RandomLogo";
 import GitHubCards from './github_cards/GitHubCards'
+import {SocialIcon} from "react-social-icons";
 
 function App() {
   // Config
@@ -10,21 +11,40 @@ function App() {
   // noinspection JSValidateTypes
   return (
     <Parallax pages={pages}>
-      <ParallaxLayer factor={2} speed={0.5}>
-      {
-        // Make a few of these background emojis
-        [...Array(emojis),].map((v, i) => (
+      {/* EMOJI BACKGROUND */}
+      <ParallaxLayer factor={2} speed={0.1}>
+        {
+          // Make a few of these background emojis
+          [...Array(emojis),].map((v, i) => (
+            <RandomLogo key={i} left={Math.random() * 70 + 10 + '%'} height="1%" top={pages / emojis * i * 100 + '%'} />
+          ))
+        }
+      </ParallaxLayer>
+      <ParallaxLayer factor={2} speed={0.40}>
+        {
+          // Make a few of these background emojis
+          [...Array(emojis),].map((v, i) => (
+            <RandomLogo key={i} left={Math.random() * 70 + 10 + '%'} height="2%" top={pages / emojis * i * 100 + '%'} />
+          ))
+        }
+      </ParallaxLayer>
+      <ParallaxLayer factor={2} speed={0.65}>
+        {
+          // Make a few of these background emojis
+          [...Array(emojis),].map((v, i) => (
             <RandomLogo key={i} left={Math.random() * 70 + 10 + '%'} height="3%" top={pages / emojis * i * 100 + '%'} />
-        ))
-      }
+          ))
+        }
       </ParallaxLayer>
 
+      {/* TITLE HEADER */}
       <ParallaxLayer speed={0.75} sticky={{start: 0, end: 0.3}} className="content">
         <h1>I like to party!!!</h1>
-        <p style={{bottom: '10%'}}>(Scroll down...)</p>
+        <p style={{bottom: '10%'}}>(Keep scrolling down...)</p>
       </ParallaxLayer>
 
-      <ParallaxLayer offset={1} speed={0.75} sticky={{start: 1, end: 1.5}} className="content">
+      {/* FIRST SLIDE: INTRO */}
+      <ParallaxLayer offset={1} speed={0.75} sticky={{start: 1, end: 2}} className="content">
         <p>
           Just kidding. I love learning new technologies and using them to help improve people's lives.
         </p>
@@ -33,11 +53,18 @@ function App() {
           My favorite hobbies are <code>programming</code> and <code>hacking / pentesting</code>.
         </p>
       </ParallaxLayer>
-      <ParallaxLayer offset={2} speed={0.75} sticky={{start: 2, end: 3}} factor={1} className="content">
+
+      {/* SECOND SLIDE: PROJECTS */}
+      <ParallaxLayer offset={2.5} speed={0.75} sticky={{start: 2.5, end: 3.5}} factor={1} className="content">
         <p>
-          Here are some of my favorite projects I've made on GitHub:
+          Here are some of my favorite projects I've made:
         </p>
         <GitHubCards />
+      </ParallaxLayer>
+
+      {/* THIRD SLIDE: LINKS */}
+      <ParallaxLayer offset={4.5} speed={0.75} sticky={{start: 4.5, end: pages}} className="content">
+        <SocialIcon url="https://github.com/Xiddoc" />
       </ParallaxLayer>
     </Parallax>
   );
