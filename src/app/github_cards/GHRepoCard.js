44d98bff6a06e7f04004f0d4e9a6cf370a46338a
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2023-01-28 17:20:15 +0200

    Make repo cards open in a new tab

--- src/app/github_cards/GHRepoCard.js
+++ src/app/github_cards/GHRepoCard.js
@@ -1,7 +1,7 @@
 
 function GHRepoCard({username, repo}) {
   return (
-    <a href={`https://github.com/${username}/${repo}`}>
+    <a target="_blank" href={`https://github.com/${username}/${repo}`}>
       <img src={
         `https://github-readme-stats.vercel.app/api/pin?theme=github_dark&show_owner=true&hide_border=true`+
         `&username=${username}&repo=${repo}`}
