ee7f06ad4c59578c2b8e6dd2fbfe1f6fcb023f1e
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2026-02-21 12:59:09 +0200

    Add explicit noreferrer for href tag

--- src/app/github_cards/GHRepoCard.js
+++ src/app/github_cards/GHRepoCard.js
@@ -1,7 +1,7 @@
 
 function GHRepoCard({username, repo}) {
   return (
-    <a target="_blank" href={`https://github.com/${username}/${repo}`}>
+    <a target="_blank" rel="noreferrer" href={`https://github.com/${username}/${repo}`}>
       <img src={`https://gh-card.dev/repos/${username}/${repo}.svg`}
         alt="GitHub repository card." height="15%"/>
     </a>
