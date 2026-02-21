f6fa59ece144fd0eb5dce61196337fa5ed1a0f11
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2026-02-21 12:41:14 +0200

    Update GitHub repo card

--- src/app/github_cards/GHRepoCard.js
+++ src/app/github_cards/GHRepoCard.js
@@ -2,9 +2,7 @@
 function GHRepoCard({username, repo}) {
   return (
     <a target="_blank" href={`https://github.com/${username}/${repo}`}>
-      <img src={
-        `https://github-readme-stats.vercel.app/api/pin?theme=github_dark&show_owner=true&hide_border=true`+
-        `&username=${username}&repo=${repo}`}
+      <img src={`https://gh-card.dev/repos/${username}/${repo}.svg`}
         alt="GitHub repository card." height="15%"/>
     </a>
   )
